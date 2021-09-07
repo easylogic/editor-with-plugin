211ff656d2533f0e938fefa82c6d88242199b392
easylogic <cyberuls@gmail.com>
2021-09-07 10:22:19 +0900

    update @easylogic/editor

--- src/dev-player.js
+++ src/dev-player.js
@@ -3,14 +3,14 @@ import 'babel-polyfill';
 import '@easylogic/editor/dist/editor.css';
 
 async function start() {
-    const player = await import (/* webpackPrefetch: true */ '@easylogic/editor/dist/player');
+    const editor = await import (/* webpackPrefetch: true */ '@easylogic/editor');
     const FeatherIconsPlugin = (await import(/* webpackPrefetch: true */ '@easylogic/editor-plugin-feather-icons')).default;
     const ColorAssetPlugin = (await import(/* webpackPrefetch: true */ '@easylogic/editor-plugin-color-asset')).default;
     const AntDesignIconsPlugin = (await import(/* webpackPrefetch: true */ '@easylogic/editor-plugin-ant-design-icons')).default;
     const PrimerOctIconsPlugin = (await import(/* webpackPrefetch: true */ '@easylogic/editor-plugin-primer-oct-icons')).default;
     const ReactPlugin = (await import(/* webpackPrefetch: true */ '@easylogic/editor-plugin-react')).default;
 
-    player.createDesignPlayer({
+    editor.createDesignEditor({
         container: document.getElementById('app'),
         plugins: [
             ColorAssetPlugin,
